4c149b3c9c0af011ee062248063fe5d8e9a7ba6c
Ukasquared <ukachukwujudith95@gmail.com>
2023-12-23 08:56:07 +0100

    fixing another programmers code

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -16,26 +16,20 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int p;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
 	saved_head = *head;
 	p = 0;
-	if (p > index)
-	{
-		return (-1);
-	}
 	while (p < index && *head != NULL)
 	{
 		*head = (*head)->next;
 		p++;
 	}
-	/* if (p != index)
+	if (p != index)
 	{
 		*head = saved_head;
 		return (-1);
-	} */
-	if (0 == index)
+	}
+	if (index == 0)
 	{
 		tmp = (*head)->next;
 		free(*head);
